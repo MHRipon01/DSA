ee7aa4764a4559286b82ea213e5df32f8da07300
MHRipon01 <mhripon012@gmail.com>
2025-01-03 00:27:21 -0800

    inserting at head and tail

---
+++ Doubly Linked List/insert_at_tail.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *prev;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+};
+
+void print_forward(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+
+void insert_at_tail(Node *&head,Node *&tail, int val)
+{
+    Node *newnode = new Node(val);
+    if(head == NULL){
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+    tail->next = newnode;
+    newnode->prev = tail;
+    newnode = tail;
+    
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *tail = new Node(30);
+
+    head->next = a;
+    a->prev = head;
+    a->next = tail;
+    tail->prev = a;
+
+    print_forward(head);
+    insert_at_tail(head,tail, 100);
+    insert_at_tail(head,tail, 200);
+    print_forward(head);
+    return 0;
+}
