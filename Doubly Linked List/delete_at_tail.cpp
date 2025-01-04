58d670330d1028fe1d516d76c5664c6451b74c6f
MHRipon01 <mhripon012@gmail.com>
2025-01-03 20:22:37 -0800

    delete from tail

---
+++ Doubly Linked List/delete_at_tail.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *prev;
+    Node *next;
+
+    // constructor
+    Node(int val)
+    {
+        this->val = val;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+};
+
+// function for print
+void print_forward(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+
+// delete from tail function
+void delete_at_tail(Node *&head, Node *&tail)
+{
+    Node *deleteNode = tail;
+    tail = tail->prev;
+    delete deleteNode;
+    if(tail == NULL){
+        head = NULL;
+        return;
+    }
+    tail->next = NULL;
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *tail = new Node(30);
+
+    // connection of nodes
+    head->next = a;
+    a->prev = head;
+    a->next = tail;
+    tail->prev = a;
+
+    print_forward(head);
+
+    delete_at_tail(head, tail);
+
+    print_forward(head);
+
+    return 0;
+}
