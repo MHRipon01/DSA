61fb73158311e78bb57d22a0f9428baa01e24ade
MHRipon01 <mhripon012@gmail.com>
2025-01-31 23:27:40 +0600

    concept of CBT

---
+++ Heap Implementation/Concept_of_Complete_Binary_Tree.cpp
@@ -0,0 +1,41 @@
+/**
+ * A CBT(Complete Binary Tree) is a type of binary tree where 
+ * 1. All levels are completely filled except possibly the last level.
+ *  
+ * 2. The last level is filled from left to right without any gaps.
+ * 
+ * Example of CBT:
+ *     1
+      / \
+     2   3
+    / \  /
+   4   5 6 
+ * 
+ *
+ * Not a CBT:
+ *     1
+      / \
+     2   3
+    / \    \
+   4   5    6
+
+ * 
+ * 
+ * Difference between CBT and Perfect Binary tree is:
+ * A Perfect Binary Tree is always complete, but a Complete Binary Tree is not always perfect.
+ * 
+ * 
+ * The height of the CBT is O(log N)
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ *  **/
