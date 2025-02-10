e4c9b194a55e5c2c1612cefec43e9fca92a648e3
MHRipon01 <mhripon012@gmail.com>
2025-02-10 12:00:00 +0600

    Maintaining streak

---
+++ Algorithm/Graph/adjacency_matrix_of_undirected_graph.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    int adj_mat[n][n];
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         adj_mat[i][j] = 0;
+    //     }
+    // }
+
+
+    //memset(mat_name,value,size of the matrix);
+    memset(adj_mat,0,sizeof(adj_mat));
+
+
+
+    for (int i = 0; i < e; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+    }
+
+    return 0;
+}
