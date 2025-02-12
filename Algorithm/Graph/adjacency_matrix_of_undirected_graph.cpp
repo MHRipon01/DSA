138bfd19a7f8044113e08227919b8f11dd6a0ba3
MHRipon01 <mhripon012@gmail.com>
2025-02-12 07:43:14 +0600

    adj mat

--- Algorithm/Graph/adjacency_matrix_of_undirected_graph.cpp
+++ Algorithm/Graph/adjacency_matrix_of_undirected_graph.cpp
@@ -17,7 +17,31 @@ int main()
 
     // memset(mat_name,value,size of the matrix);
     memset(adj_mat, 0, sizeof(adj_mat));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+                adj_mat[i][j] = 1;
+        }
+    }
 
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         cout << adj_mat[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    for (int i = 0; i < e; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj_mat[a][b] = 1;
+        adj_mat[b][a] = 1; // for undirected graph
+    }
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
@@ -26,12 +50,5 @@ int main()
         }
         cout << endl;
     }
-
-    for (int i = 0; i < e; i++)
-    {
-        int a, b;
-        cin >> a >> b;
-    }
-
     return 0;
 }
