8ae3386690662d70b808e8892901d81ce4a6fe7b
MHRipon01 <mhripon012@gmail.com>
2025-03-02 14:27:50 +0600

    time complexity

--- Data_structure/Complexity/sqrt_complexity.cpp
+++ Data_structure/Complexity/sqrt_complexity.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cin >> n;
+    int n; //O(1)
+    cin >> n; //O(1)
 
-    for (int i = 1; i <= sqrt(n); i++)
+    for (int i = 1; i <= sqrt(n); i++) // O(sqrt N)
     {
         cout << i << endl;
     }
