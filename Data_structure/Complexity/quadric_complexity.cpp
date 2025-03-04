08fc78d7b7e3c0d40234fddc0292015f752e8359
MHRipon01 <mhripon012@gmail.com>
2025-03-04 11:32:05 +0600

    complexity analysis

--- Data_structure/Complexity/quadric_complexity.cpp
+++ Data_structure/Complexity/quadric_complexity.cpp
@@ -5,13 +5,15 @@ int main()
 {
     int n;
     cin >> n;
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)// O(N)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < n; j++)// O(N)
         {
-            cout << "Hello " << endl;
+            cout << "Hello " << endl; 
         }
     }
 
     return 0;
 }
+
+// Total complexity: O(n*n)
