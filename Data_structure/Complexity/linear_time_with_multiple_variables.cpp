4df00795e5e0c40bc45a3ee23b9353d013f8411c
MHRipon01 <mhripon012@gmail.com>
2025-03-05 15:03:32 +0600

    O(m+n)

---
+++ Data_structure/Complexity/linear_time_with_multiple_variables.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int m, n;
+    cin >> m >> n;
+    for (int i = 0; i < m; i++)
+    { // First loop
+        cout << "i = " << i<< " for first loop" << endl;
+    }
+    for (int j = 0; j < n; j++)
+    { // Second loop
+        cout << "j = " << j<< " for second loop" << endl;
+    }
+
+    return 0;
+}
